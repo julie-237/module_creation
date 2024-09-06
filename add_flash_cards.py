4a443995e224b015d5d4b2a60debbbe7ee561ee9
unknown <blandinedjoumsie@gmail.com>
2024-09-06 16:30:08 +0200

    wrote the obtained list in the current file, then created another file called study_flash_cards in which i read the list from the previous file

--- add_flash_cards.py
+++ add_flash_cards.py
@@ -16,8 +16,10 @@ while new_card == "yes":
 print(card_list)
 save_to_file = input("do you want to save cards to file? (yes/no) ")
 if save_to_file == "yes":
-    
+    with open("add_flash_cards.py", "a") as f:
+        f.write(str(card_list))
     print("saving...")
 else:
     exit
 print("End of program")
+['amour', 'femme', 'lit']
---
+++ study_flash_cards.py
@@ -0,0 +1,4 @@
+with open("add_flash_cards.py", "r") as f:
+    content = f.read
+    print(content)
+print("The current card_list contains " + len(content) + "words")
