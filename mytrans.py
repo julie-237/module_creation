e658fffa4e44e5ff1da83b06a1debbe03925a467
unknown <blandinedjoumsie@gmail.com>
2024-09-06 14:19:47 +0200

    created a file(mytrans.py), then created a function in it and added 10 words and their translations

---
+++ mytrans.py
@@ -0,0 +1,21 @@
+def english_to_french_translator(text):
+    if text == "school":
+        return "école"
+    elif text == "kitchen":
+        return"cuisine"
+    elif text == "chiken":
+        return"poulet"
+    elif text == "bag":
+        return"sac"
+    elif text == "bed":
+       return"lit"
+    elif text == "church":
+        return"eglise"
+    elif text == "man":
+        return"homme"
+    elif text == "woman":
+      return"femme"
+    elif text == "slave":
+        return"esclave"
+    elif text == "love":
+        return"amour"
